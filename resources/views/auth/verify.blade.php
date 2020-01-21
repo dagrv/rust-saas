c53d660119b4a3a76e1816fcd855173cb014b6ea
rust <me@rustx.co>
2020-01-21 16:51:00 +0100

    Add: Interface Styling Login, Signup, Reset password

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,33 +1,35 @@
-@extends('layouts.app')
+@extends('app')
 
 @section('content')
-    <div class="container mx-auto">
-        <div class="flex flex-wrap justify-center">
-            <div class="w-full max-w-sm">
+    <div class="flex h-screen bg-gray-100 p-4 rotate">
+        <div class="sm:max-w-xl md:max-w-2xl w-full m-auto">
 
-                @if (session('resent'))
-                    <div class="text-sm border border-t-8 rounded text-green-700 border-green-600 bg-green-100  px-3 py-4 mb-4" role="alert">
-                        {{ __('A fresh verification link has been sent to your email address.') }}
-                    </div>
-                @endif
+            @if (session('resent'))
+                <div class="text-sm border border-t-8 rounded text-green-700 border-green-600 bg-green-100  px-3 py-4 mb-4" role="alert">
+                    {{ __('A fresh verification link has been sent to your email address.') }}
+                </div>
+            @endif
 
-                <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
-                    <div class="font-semibold bg-gray-200 text-gray-700 py-3 px-6 mb-0">
-                        {{ __('Verify Your Email Address') }}
-                    </div>
+            <div class="flex items-stretch bg-white rounded-lg shadow-lg overflow-hidden border-t-4 border-indigo-500 sm:border-0">
+                
+                <div class="flex hidden overflow-hidden relative sm:block w-5/12 md:w-6/12 bg-gray-600 text-gray-300 py-4 bg-cover bg-center" style="background-image: url('/img/paris.jpg')">
+                    <div class="flex-1 absolute bottom-0 text-white p-10"></div>
+                    <svg class="absolute animate h-full w-4/12 sm:w-2/12 right-0 inset-y-0 fill-current text-white" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+                        <polygon points="0,0 100,100 100,0">
+                    </svg>
+                </div>
+                
+                <div class="flex-1 p-6 sm:p-10 sm:py-12">
+                    <h3 class="text-xl text-gray-700 font-bold mb-2">Before proceeding,</h3>
+                    <p class="text-gray-700 mb-6">please check your email for a verification link.</p>
 
-                    <div class="w-full flex flex-wrap p-6">
-                        <p class="leading-normal">
-                            {{ __('Before proceeding, please check your email for a verification link.') }}
-                        </p>
-
-                        <p class="leading-normal mt-6">
-                            {{ __('If you did not receive the email') }}, <a class="text-blue-500 hover:text-blue-700 no-underline" onclick="event.preventDefault(); document.getElementById('resend-verification-form').submit();">{{ __('click here to request another') }}</a>.
-                        </p>
-
-                        <form id="resend-verification-form" method="POST" action="{{ route('verification.resend') }}" class="hidden">
-                            @csrf
-                        </form>
+                    <div class="flex flex-wrap items-center">
+                        <div class="w-full sm:flex-1">
+                            <form action="{{ route('verification.resend') }}" method="POST">
+                                @csrf
+                                <input type="submit" value="Resend verification email" class="w-full sm:w-auto bg-green-500 text-white px-6 py-2 rounded hover:bg-indigo-600 focus:outline-none cursor-pointer">
+                            </form>
+                        </div>
                     </div>
 
                 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,35 +6,7 @@
 
     <div class="h-2 w-full bg-indigo-600 absolute z-20 gradient"></div>
 
-    <div class="absolute w-full z-20">
-        <nav class="font-sans text-center flex justify-between my-4 mx-auto container overflow-hidden mt-8 z-50 relative px-5">
-            <a href="/" class="toggleColour text-gray-700 no-underline hover:no-underline font-bold text-2xl lg:text-4xl">
-                <img src="/img/logo.png" alt="We make SaaS products" class="h-8">
-            </a>
-            
-            <ul class="text-sm text-grey-dark list-reset flex items-center">
-                <li class="mr-8">
-                    <a href="/" class="text-orange-400 rounded font-extrabold hidden sm:block">Home</a>
-                </li>
-                
-                <li class="mr-8">
-                    <a href="#" class="text-black hover:text-orange-400 rounded font-bold hidden sm:block">Feautures</a>
-                </li>
-
-                <li class="mr-8">
-                    <a href="#" class="text-black hover:text-orange-400 rounded font-bold hidden sm:block">Pricing</a>
-                </li>
-
-                <li class="mr-8">
-                    <a href="#" class="text-black hover:text-orange-400 rounded font-bold hidden sm:block">F.A.Q</a>
-                </li>
-
-                <li>
-                    <a href="{{ route('login') }}" class="text-white ml-4 py-2 px-6 rounded font-bold hidden sm:block bg-green-400">Login</a>
-                </li>
-            </ul>
-        </nav>
-    </div>
+    @include('partials.nav')
 
     <h1 class="text-white">.</h1>
 
