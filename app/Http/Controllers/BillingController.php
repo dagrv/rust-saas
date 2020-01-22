f4de349ccd45c6520f9e7a783d552a423f519eba
rust <me@rustx.co>
2020-01-22 16:59:49 +0100

    Add: Update default payment method

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -2,28 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Exception;
 use Illuminate\Http\Request;
 
 class BillingController extends Controller
 {
-    
-    public function billing(Request $request)
-    {
+    public function billing (Request $request) {
         return view('settings.billing');
     }
 
-    // Submission
-    public function billing_save(Request $request)
-    {
+    public function billing_save (Request $request){
         $user = auth()->user();
-        
+
         try {
-            $user->newSubscription('default', 'basic')->create($request->payment_method);
-        } catch(Exception $e) {
-            return back()->with(['alert' => 'Something went wrong while updating your Credit Card !', 'alert_type' => 'error']);
+            if ($user->subscribed('main')) {
+                // update their credit card
+                $user->updateDefaultPaymentMethod($request->payment_method);
+            } else {
+                $user->newSubscription('main', 'basic')->create($request->payment_method);
+            }
+        } catch(Exception $e){
+            return back()->with(['alert' => 'Something went wrong submitting your billing info', 'alert_type' => 'error']);
         }
 
-        return back()->with(['alert' => 'Successfully Updated your Credit Card !', 'alert_type' => 'success']);
+        return back()->with(['alert' => 'Successfully updated your billing info', 'alert_type' => 'success']);
     }
 }
--- resources/views/settings/billing.blade.php
+++ resources/views/settings/billing.blade.php
@@ -26,7 +26,23 @@
                         <h2 class="font-medium text-md text-gray-700 mb-4 tracking-wide">Billing Settings</h2>
                         <p class="text-xs text-gray-500">Update your payment informations</p>
                     </div>
+                    
                     <div class="md:w-2/3 w-full">
+                        <div class="py-8 px-16">
+
+                            @if(auth()->user()->subscribed('main'))
+                                <div class="py-6 px-2">
+                                    <div class="mb-2 text-normal">🎉 Thanks for being a subscriber ! 🎊</div>
+
+                                    <div class="text-sm text-blue-600 mb-1">Last 4 digits:  {{ auth()->user()->card_last_four }}</div>
+                                    <div class="text-sm text-blue-600 mb-1">Credit Card Brand:  {{ auth()->user()->card_brand }}</div>
+                                    <div class="text-xs text-gray-500">To update your default payment method, Add a new card below</div>
+                                </div>                                
+                            @endif
+
+                        </div>
+                        <hr class="border-gray-200">
+                        
                         <div class="py-8 px-16">
                             <label for="card-holder-name" class="text-sm text-gray-600">Name on your Credit Card </label>
                             <input class="mt-2 border-2 border-gray-200 px-3 py-2 block w-full rounded-lg text-base text-gray-900 focus:outline-none focus:border-indigo-500" id="card-holder-name" type="text">
